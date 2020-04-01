896cdeefd7fee7f3aebcca8d567d0f548700961d
shoreychris <58793322+shoreychris@users.noreply.github.com>
2020-04-01 13:46:26 +0100

    Add files via upload

---
+++ gallery1.php
@@ -0,0 +1,44 @@
+<html>
+<?php include "topper.php";?>
+<head>
+    <link rel="stylesheet" href="HD2.css" type="text/css">
+</head>
+<body>
+
+<div class=content>
+
+    <h2><center>Grooming</center></h2>
+
+
+    <img src="WSPics/gallery3.JPG" width= "500" alt="error">
+    <img src="WSPics/gallery2.jpg" width= "500" alt="error"><br>
+    <img src="WSPics/gallery5.jpg" width= "500" alt="error">
+    <img src="WSPics/gallery4.jpg" width= "500" alt="error"><br>
+    <img src="WSPics/gallery6.jpg" width= "500" alt="error">
+    <img src="WSPics/gallery7.jpeg" width= "500" alt="error"><br>
+
+
+
+
+
+
+
+
+
+
+
+<br>
+<br>
+<div class="container">
+    <br>
+    <?php
+include "media.php";
+?>
+</div>
+
+<?php
+include "base.php";
+?>
+</div>
+</body>
+</html>
---
+++ gallery2.php
@@ -0,0 +1,43 @@
+<html>
+<?php include "topper.php";?>
+<head>
+    <link rel="stylesheet" href="HD2.css" type="text/css">
+</head>
+<body>
+
+<div class=content>
+
+    <h2><center>Daycare</center></h2>
+
+
+    <img src="WSPics/daycare1.jpg" width= "500" alt="error">
+    <img src="WSPics/daycare2.jpg" width= "500" alt="error">
+    <img src="WSPics/daycare3.jpg" width= "500" alt="error">
+    <img src="WSPics/daycare4.jpg" width= "500" alt="error">
+
+
+
+
+
+
+
+
+
+
+
+
+<br>
+<br>
+<div class="container">
+    <br>
+    <?php
+include "media.php";
+?>
+</div>
+
+<?php
+include "base.php";
+?>
+</div>
+</body>
+</html>
